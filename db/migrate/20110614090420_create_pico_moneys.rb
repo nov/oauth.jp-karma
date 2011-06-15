2f9164406e3a10fbf6556666322f42c49348daf6
nov matake <nov@matake.jp>
2011-06-15 11:33:49 +0900

    Revert ""_token_" didn't work.."
    This reverts commit 62272cfcc1bfb75df4bc9d346578474257560e26.

--- db/migrate/20110614090420_create_pico_moneys.rb
+++ db/migrate/20110614090420_create_pico_moneys.rb
@@ -2,7 +2,7 @@ class CreatePicoMoneys < ActiveRecord::Migration
   def change
     create_table :pico_moneys do |t|
       t.belongs_to :account
-      t.string :access_token, :access_token_secret, :identifier, :profile, :thumbnail
+      t.string :_token_, :secret, :identifier, :profile, :thumbnail
       t.timestamps
     end
   end
