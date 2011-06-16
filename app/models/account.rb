8c1bf0305b89e9048a9a47877891bba2b1616ff3
nov matake <nov@matake.jp>
2011-06-16 15:29:16 +0900

    no signup bonus for now

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,19 +1,4 @@
 class Account < ActiveRecord::Base
   has_one :pico_money
   has_many :transactions
-
-  after_create :signup_bonus
-
-  SIGNUP_BONUS = 100
-  def signup_bonus
-    if pico_money.email_md5
-      PicoMoney.issuer.account.transactions.create!(
-        to: pico_money.email_md5,
-        amount: SIGNUP_BONUS,
-        description: ''
-      )
-    end
-  rescue OpenTransact::HttpException
-    # ignore
-  end
 end
