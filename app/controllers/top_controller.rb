dc7f6e87d295a63b2b7e25301bacc1e7aba934b2
nov matake <nov@matake.jp>
2011-06-17 18:25:39 +0900

    remove debug code

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,10 +2,5 @@ class TopController < ApplicationController
   before_filter :require_anonymous_access
 
   def index
-    flash[:notice] = {
-      title: 'flash.title.welcome'.t(identifier: 'nov'),
-      text:  'flash.description.welcome'.t,
-      image: PicoMoney.first.thumbnail
-    }
   end
 end
