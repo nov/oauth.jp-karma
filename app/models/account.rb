31a10af1d3a9c369554272e38fc6d38c7597e475
nov matake <nov@matake.jp>
2011-06-17 19:02:39 +0900

    signup bonus again

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,19 @@
 class Account < ActiveRecord::Base
   has_one :pico_money
   has_many :transactions
+
+  after_create :signup_bonus
+
+  SIGNUP_BONUS = 100
+  def signup_bonus
+    if pico_money.email_md5
+      PicoMoney.issuer.account.transactions.create!(
+        to: pico_money.email_md5,
+        amount: SIGNUP_BONUS,
+        description: 'OAuth.jp Karma Signup Bonus'
+      )
+    end
+  rescue OpenTransact::HttpException
+    # ignore
+  end
 end
