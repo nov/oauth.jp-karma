48722be5efff6cb7120a1dbdc6ca4b178cf4fcc2
nov matake <nov@matake.jp>
2011-06-17 17:57:46 +0900

    hum

--- app/helpers/meta_data_helper.rb
+++ app/helpers/meta_data_helper.rb
@@ -1,9 +1,9 @@
 module MetaDataHelper
-  def meta_tag(name, options = {})
-    tag :meta, options.merge(name: name)
+  def meta_tag(name, content, options = {})
+    tag :meta, options.merge(name: name, content: content)
   end
 
-  def link_tag(rel, options = {})
-    tag :link, options.merge(rel: rel)
+  def link_tag(rel, href, options = {})
+    tag :link, options.merge(rel: rel, href: href)
   end
 end
--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -3,21 +3,21 @@ module MobileHelper
 
   module Apple
     def apple_app_capable_meta_tag
-      meta_tag :'apple-mobile-web-app-capable'
+      meta_tag 'apple-mobile-web-app-capable', 'yes'
     end
 
     def apple_app_icon_link_tag
-      link_tag :'apple-touch-icon', href: image_path('icon.png')
+      link_tag 'apple-touch-icon', image_path('icon.png')
     end
   end
   include Apple
 
   def viewport_meta_tag
-    meta_tag :viewport, content: [
+    meta_tag :viewport, [
       'width=device-width',
-      'initial-scale=1',
-      'maximum-scale=1',
-      'minimum-scale=1'
+      'initial-scale=1.0',
+      'maximum-scale=1.0',
+      'minimum-scale=1.0'
     ].join(',')
   end
 
